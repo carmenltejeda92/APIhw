a84654bd0e065eae6c7c6a058e518c64eee62669
carmenltejeda92 <carmenltejeda92@gmail.com>
2023-11-28 06:05:36 -0500

    Initial Commit

---
+++ src/components/Form.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Form() {
+  return (
+    <div className='form'>
+        <div className='topDiv'>
+            <div><img></img></div>
+            <div><button id="search">Search</button></div>
+        </div>
+    </div>
+  )
+}
+
+export default Form
